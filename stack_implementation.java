32be85e27675eeb2c3598f501db572fe1b29843e
Subash Poudel <me.subash93@gmail.com>
2023-09-05 19:39:01 +0200

    Update stack_implementation.java

--- stack_implementation.java
+++ stack_implementation.java
@@ -1,76 +1,81 @@
-import static java.lang.System.out;
+public class Stack {
+    private int maxSize;
+    private int top;
+    private int[] stackArray;
 
-class Stack {
-	private static class Node {
-		private int data;
-		private Node next;
-		
-		public Node(int data) {
-			this.data = data;
-		}
-		
-		public int getData() {
-			return data;
-		}
-		
-		public void setData(int data) {
-			this.data = data;
-		}
-		
-		public Node getNext() {
-			return next;
-		}
-		
-		public void setNext(Node next) {
-			this.next = next;
-		}
-	}
-	
-	private Node top;
-	private int count;
-	
-	public Stack() {
-		top = null;
-		count = 0;
-	}
-	
-	public void push(Node n) {
-		if(n != null) {
-			n.setNext(top);
-			top = n;
-			count++;
-		}
-	}
-	
-	public Node peek() {
-		return top;
-	}
-	
-	public Node pop() {
-		Node k = top;
-		if(top != null) {
-			top = top.getNext();
-			count--;
-		}
-		return k;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		Stack stack = new Stack();
-		
-		for(int i = 0; i < 50; i++) {
-			stack.push(new Node(i));
-		}
-		
-		out.println("Count: " + stack.getCount());
-		
-		Node k;
-		while((k = stack.pop()) != null) {
-			out.println(k.getData() + " ");
-		}
-	}
+    // Constructor to initialize the stack with a specified size
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; // Initialize top to -1 to indicate an empty stack
+    }
+
+    // Check if the stack is empty
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    // Check if the stack is full
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    // Push an element onto the stack
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push " + value);
+        } else {
+            stackArray[++top] = value;
+        }
+    }
+
+    // Pop an element from the stack
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop.");
+            return -1; // Return a sentinel value to indicate an error
+        } else {
+            return stackArray[top--];
+        }
+    }
+
+    // Peek at the top element of the stack without removing it
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek.");
+            return -1; // Return a sentinel value to indicate an error
+        } else {
+            return stackArray[top];
+        }
+    }
+
+    // Get the number of elements in the stack
+    public int size() {
+        return top + 1;
+    }
+
+    // Display the elements in the stack
+    public void display() {
+        System.out.print("Stack: ");
+        for (int i = 0; i <= top; i++) {
+            System.out.print(stackArray[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        stack.display();
+
+        System.out.println("Pop: " + stack.pop());
+        System.out.println("Peek: " + stack.peek());
+
+        stack.display();
+    }
 }
