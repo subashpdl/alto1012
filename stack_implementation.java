6023982048ad6166edc1bd369fa918e6c6f2adfe
Subash Poudel <me.subash93@gmail.com>
2023-09-05 14:15:57 +0200

    Update stack_implementation.java

--- stack_implementation.java
+++ stack_implementation.java
@@ -1,67 +1,76 @@
-
-import java.util.NoSuchElementException;
+import static java.lang.System.out;
 
 class Stack {
-    protected int arr[];
-    protected int size;
-    protected int top;
-
-    public Stack(int n) {
-        size = n;
-        arr = new int[n];
-        top = -1;
-    }
-
-    boolean isEmpty() {
-        return top == -1;
-    }
-
-    void Push(int x) {
-        if (top >= size - 1) {
-            throw new IndexOutOfBoundsException("Overflow Exception");
-        }
-        top++;
-        arr[top] = x;
-    }
-
-    void Pop() {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Underflow Exception");
-        } else {
-            top--;
-        }
-    }
-
-    int Top() {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Underflow Exception");
-        } else
-            return arr[top];
-    }
-
-    void display() {
-        if (isEmpty()) {
-            System.out.print("Empty");
-        } else {
-            for (int i = 0; i <= top; i++) {
-                System.out.print(arr[i] + "\t");
-            }
-        }
-    }
-
-}
-
-public class Reverse {
-    public static void main(String[] args) {
-        Stack s = new Stack(5);
-        s.Push(4);
-        s.Push(6);
-        s.Push(5);
-        s.Push(8);
-        s.Push(9);
-        s.Pop();
-        s.display();
-
-    }
-
+	private static class Node {
+		private int data;
+		private Node next;
+		
+		public Node(int data) {
+			this.data = data;
+		}
+		
+		public int getData() {
+			return data;
+		}
+		
+		public void setData(int data) {
+			this.data = data;
+		}
+		
+		public Node getNext() {
+			return next;
+		}
+		
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
+	
+	private Node top;
+	private int count;
+	
+	public Stack() {
+		top = null;
+		count = 0;
+	}
+	
+	public void push(Node n) {
+		if(n != null) {
+			n.setNext(top);
+			top = n;
+			count++;
+		}
+	}
+	
+	public Node peek() {
+		return top;
+	}
+	
+	public Node pop() {
+		Node k = top;
+		if(top != null) {
+			top = top.getNext();
+			count--;
+		}
+		return k;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Stack stack = new Stack();
+		
+		for(int i = 0; i < 50; i++) {
+			stack.push(new Node(i));
+		}
+		
+		out.println("Count: " + stack.getCount());
+		
+		Node k;
+		while((k = stack.pop()) != null) {
+			out.println(k.getData() + " ");
+		}
+	}
 }
