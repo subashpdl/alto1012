80b01bd7a7ad589a5c824963551de7770e14577d
Subash Poudel <me.subash93@gmail.com>
2023-09-06 13:51:24 +0200

    Update stack.java

--- stack.java
+++ stack.java
@@ -1,82 +1,79 @@
-import java.util.EmptyStackException;
+public class SimpleStack {
+    private int maxSize;         // Maximum size of the stack
+    private int top;             // Index of the top element
+    private int[] stackArray;    // Array to store stack elements
 
-interface Stack {
-    Object push(Object item);
-    Object pop();
-    Object peek();
-    int size();
-}
+    // Constructor to initialize the stack with a specified size
+    public SimpleStack(int size) {
+        maxSize = size;
+        stackArray = new int[maxSize];
+        top = -1;  // Initialize top as -1 to indicate an empty stack
+    }
 
-class SimpleStack implements Stack {
-    private Node top;
-    private int count;
-
-    private class Node {
-        Object data;
-        Node next;
-
-        Node(Object data) {
-            this.data = data;
-            this.next = null;
+    // Push an element onto the stack
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push.");
+        } else {
+            stackArray[++top] = value;
         }
     }
 
-    public SimpleStack() {
-        this.top = null;
-        this.count = 0;
-    }
-
-    @Override
-    public Object push(Object item) {
-        Node newNode = new Node(item);
-        newNode.next = top;
-        top = newNode;
-        count++;
-        return item;
-    }
-
-    @Override
-    public Object pop() {
+    // Pop and return the top element from the stack
+    public int pop() {
         if (isEmpty()) {
-            throw new EmptyStackException();
+            System.out.println("Stack is empty. Cannot pop.");
+            return -1; // Return a sentinel value for an empty stack
+        } else {
+            return stackArray[top--];
         }
-        Object data = top.data;
-        top = top.next;
-        count--;
-        return data;
     }
 
-    @Override
-    public Object peek() {
+    // Peek at the top element without removing it from the stack
+    public int peek() {
         if (isEmpty()) {
-            throw new EmptyStackException();
+            System.out.println("Stack is empty. Cannot peek.");
+            return -1; // Return a sentinel value for an empty stack
+        } else {
+            return stackArray[top];
         }
-        return top.data;
     }
 
-    @Override
-    public int size() {
-        return count;
-    }
-
+    // Check if the stack is empty
     public boolean isEmpty() {
-        return top == null;
+        return top == -1;
+    }
+
+    // Check if the stack is full
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    // Get the number of elements in the stack
+    public int size() {
+        return top + 1;
     }
 
     public static void main(String[] args) {
-        Stack stack = new SimpleStack();
+        // Create a stack with a maximum size of 5
+        SimpleStack stack = new SimpleStack(5);
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
+        // Push elements onto the stack
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
 
-        System.out.println("Stack size: " + stack.size());
+        // Pop and print elements
+        System.out.println("Popped element: " + stack.pop()); // Should print 30
+        System.out.println("Popped element: " + stack.pop()); // Should print 20
 
-        System.out.println("Peek: " + stack.peek());
+        // Peek at the top element
+        System.out.println("Top element: " + stack.peek()); // Should print 10
 
-        System.out.println("Pop: " + stack.pop());
-        System.out.println("Pop: " + stack.pop());
+        // Check if the stack is empty
+        System.out.println("Is the stack empty? " + stack.isEmpty()); // Should print false
 
-        System.out.println("Stack size after pops: " + stack.size());
+        // Get the number of elements in the stack
+        System.out.println("Number of elements in the stack: " + stack.size()); // Should print 1
     }
 }
