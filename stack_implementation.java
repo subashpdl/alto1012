7cc0fda1a6389a6520c1dd4e7b2b886ae23113dc
Subash Poudel <me.subash93@gmail.com>
2023-09-06 12:54:30 +0200

    Update stack_implementation.java

--- stack_implementation.java
+++ stack_implementation.java
@@ -1,81 +1,68 @@
-public class Stack {
-    private int maxSize;
-    private int top;
-    private int[] stackArray;
+import java.util.EmptyStackException;
 
-    // Constructor to initialize the stack with a specified size
-    public Stack(int size) {
-        this.maxSize = size;
-        this.stackArray = new int[maxSize];
-        this.top = -1; // Initialize top to -1 to indicate an empty stack
-    }
+public class SimpleStack {
+    private Node top;
 
-    // Check if the stack is empty
-    public boolean isEmpty() {
-        return top == -1;
-    }
+    private class Node {
+        int data;
+        Node next;
 
-    // Check if the stack is full
-    public boolean isFull() {
-        return top == maxSize - 1;
-    }
-
-    // Push an element onto the stack
-    public void push(int value) {
-        if (isFull()) {
-            System.out.println("Stack is full. Cannot push " + value);
-        } else {
-            stackArray[++top] = value;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
-    // Pop an element from the stack
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
     public int pop() {
         if (isEmpty()) {
-            System.out.println("Stack is empty. Cannot pop.");
-            return -1; // Return a sentinel value to indicate an error
-        } else {
-            return stackArray[top--];
+            throw new EmptyStackException();
         }
+        int data = top.data;
+        top = top.next;
+        return data;
     }
 
-    // Peek at the top element of the stack without removing it
     public int peek() {
         if (isEmpty()) {
-            System.out.println("Stack is empty. Cannot peek.");
-            return -1; // Return a sentinel value to indicate an error
-        } else {
-            return stackArray[top];
+            throw new EmptyStackException();
         }
+        return top.data;
     }
 
-    // Get the number of elements in the stack
-    public int size() {
-        return top + 1;
-    }
-
-    // Display the elements in the stack
-    public void display() {
+    public void printStack() {
+        Node current = top;
         System.out.print("Stack: ");
-        for (int i = 0; i <= top; i++) {
-            System.out.print(stackArray[i] + " ");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        Stack stack = new Stack(5);
+        SimpleStack stack = new SimpleStack();
 
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        stack.push(4);
 
-        stack.display();
+        stack.printStack();
 
-        System.out.println("Pop: " + stack.pop());
         System.out.println("Peek: " + stack.peek());
 
-        stack.display();
+        System.out.println("Pop: " + stack.pop());
+        System.out.println("Pop: " + stack.pop());
+
+        stack.printStack();
     }
 }
